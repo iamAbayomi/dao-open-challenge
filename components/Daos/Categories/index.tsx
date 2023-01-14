a4f51bb2cbefb1be78dfa15cb5106a90181e912d
iamAbayomi <oladinifolawiyo@gmail.com>
2023-01-14 04:52:14 +0100

    style: resize the DAO components

--- components/Daos/Categories/index.tsx
+++ components/Daos/Categories/index.tsx
@@ -8,10 +8,13 @@ interface props {
 const Categories = ({ item }: props) => {
   return (
     <Box
-      mb={"15px"}
+      height={"150px"}
+      width={"100%"}
+      max-width={"150px"}
       p={"5px 10px"}
       background={item?.background}
       borderRadius={"10px"}
+      overflow={"auto"}
     >
       <Text fontWeight={"400"} fontSize={"18px"}>
         {item?.title}
